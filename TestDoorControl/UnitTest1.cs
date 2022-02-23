d94cf3314862a63ef35750fd8aa9e2004ccdbd64
ApolloISE <mads.apollo@gmail.com>
2022-02-23 16:37:39 +0100

    Add project files.

---
+++ TestDoorControl/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestDoorControl
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
