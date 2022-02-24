ac63264ba5dfa52d65b9a74e7020373eb3ac0381
ApolloISE <mads.apollo@gmail.com>
2022-02-24 14:03:35 +0100

    adedd functionality

--- FakeDoorControl/FakeAlarm.cs
+++ FakeDoorControl/FakeAlarm.cs
@@ -4,12 +4,11 @@ namespace FakeDoorControl
 {
     public class FakeAlarm : IAlarm
     {
-        private bool alarmIsOn = false;
         public bool AlarmIsOn
         {
             get;
             private set;
-        }
+        } = false;
 
         public void RaiseAlarm()
         {
