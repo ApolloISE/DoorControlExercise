18bd75924bcd1003203deb9c581559fcc0838951
KlementSvaneB <klementsvane@gmail.com>
2022-02-24 13:39:45 +0100

    oprettelse af setup

--- TestDoorControl/UnitTest1.cs
+++ TestDoorControl/UnitTest1.cs
@@ -1,12 +1,25 @@
+using DoorControlExercise;
+using FakeDoorControl;
 using NUnit.Framework;
 
 namespace TestDoorControl
 {
+
     public class Tests
     {
+        IAlarm? _alarm;
+        IDoor? _door;
+        IEntryNotification? _entryNotification;
+        IUserValidation? _userValidation;
+        private DoorControl uut;
         [SetUp]
         public void Setup()
         {
+            _alarm = new FakeAlarm();
+            _door = new FakeDoor();
+            _entryNotification = new FakeEntryNotification();
+            _userValidation = new UserValidation();
+            uut = new DoorControl(_alarm, _door, _entryNotification, _userValidation);
         }
         
         [Test]
@@ -14,5 +27,6 @@ namespace TestDoorControl
         {
             Assert.Pass();
         }
+
     }
 }
